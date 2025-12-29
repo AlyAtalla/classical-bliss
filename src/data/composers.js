7737124af3be22dcb3c9f86b6bd797c923dc9533
AlyAtalla <aliatalla93@gmail.com>
2025-12-29 22:10:01 +0200

    install router

--- src/data/composers.js
+++ src/data/composers.js
@@ -4,27 +4,47 @@ export const composers = [
     name: "Johann Sebastian Bach",
     born: 1685,
     died: 1750,
-    slug: "johann_sebastian_bach"
+    slug: "johann_sebastian_bach",
+    imslp: "https://imslp.org/wiki/Category:Bach,_Johann_Sebastian",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg",
+    bio:
+      "Johann Sebastian Bach was a German composer and musician of the Baroque period. He is regarded as one of the greatest composers of all time, known for his mastery of counterpoint and harmonic organization."
   },
   {
     id: "mozart",
     name: "Wolfgang Amadeus Mozart",
     born: 1756,
     died: 1791,
-    slug: "wolfgang_amadeus_mozart"
+    slug: "wolfgang_amadeus_mozart",
+    imslp: "https://imslp.org/wiki/Category:Mozart,_Wolfgang_Amadeus",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/1/1e/Wolfgang-amadeus-mozart_1.jpg",
+    bio:
+      "Wolfgang Amadeus Mozart was a prolific and influential composer of the Classical period. He composed over 600 works, many of which are among the most admired in the repertoire."
   },
   {
     id: "beethoven",
     name: "Ludwig van Beethoven",
     born: 1770,
     died: 1827,
-    slug: "ludwig_van_beethoven"
+    slug: "ludwig_van_beethoven",
+    imslp: "https://imslp.org/wiki/Category:Beethoven,_Ludwig_van",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6f/Beethoven.jpg",
+    bio:
+      "Ludwig van Beethoven was a German composer and pianist. His work marks the transition between the Classical and Romantic eras and remains among the most frequently performed music."
   },
   {
     id: "chopin",
     name: "Frédéric Chopin",
     born: 1810,
     died: 1849,
-    slug: "frederic_chopin"
+    slug: "frederic_chopin",
+    imslp: "https://imslp.org/wiki/Category:Chopin,_Fr%C3%A9d%C3%A9ric",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/9f/Chopin%2C_by_Wodzinska.JPG",
+    bio:
+      "Frédéric Chopin was a Polish composer and virtuoso pianist of the Romantic era, known primarily for his solo piano works."
   }
 ];
